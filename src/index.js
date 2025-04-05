2f86c21204f5d87e81917c60442a93ff0483635f
Challamsetty-venkata-sai <venkatasai200410@gmail.com>
2025-04-05 13:44:25 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom/client';
+import {UsePopcorn} from './UsePopcorn/App.js'
+import './UsePopcorn/App.css'
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <UsePopcorn/>
+)
